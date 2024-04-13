f8d822eff00e848a38b51ccc4080294c2790841e
echsimo <echsimo@gmail.com>
2024-04-13 19:47:24 +0200

    First push

--- sooters-rental-app/src/main.js
+++ sooters-rental-app/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import router from "./router";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// Suppression de l'importation d'Axios
+
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
